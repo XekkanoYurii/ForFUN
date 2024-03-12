a95cce848fa7be6a694142ba5555d1916cae35d2
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-03-12 19:21:25 +0100

    Update jj.js

--- jj.js
+++ jj.js
@@ -1,6 +1,9 @@
-// const a = 50;
-// const b = 60;
-// console.log(a < b);
-const a = 50;
-const b = 3;
-console.log(a * b);
+function hello() {
+    console.log('hello', this)
+}
+const person = {
+    name: 'Max',
+    age: 25,
+    sayHello: hello
+} 
+console.log(person);
