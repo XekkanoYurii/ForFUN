4f360386b0bde3554d5bf6b47a2601a8ee485d85
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-03-27 19:31:50 +0100

    Update jj.js

--- jj.js
+++ jj.js
@@ -1,4 +1,6 @@
-function add(prise, items) {
-	console.log(items * prise);
-}
-add(115, 23)
+const mus = ['Yura', 'Olena', 'Witalia'];
+const firstElement = mus[0];
+const midlElement = mus.index[1];
+const lastElement = mus.length - 1;
+
+console.log(lastElement);
