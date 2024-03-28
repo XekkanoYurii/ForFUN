75bacffcfeae587e3771756549b70d54b49ee141
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-03-28 17:31:12 +0100

    Update jj.js

--- jj.js
+++ jj.js
@@ -1,6 +1,6 @@
-const mus = ['Yura', 'Olena', 'Witalia'];
-const firstElement = mus[0];
-const midlElement = mus.index[1];
-const lastElement = mus.length - 1;
-
-console.log(lastElement);
+function slugify(title) {
+    let toLowerCaseTitle = title.toLowerCase();
+    let slug = toLowerCaseTitle.split(' ').join('-')
+    return slug
+   
+}
