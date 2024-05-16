2d2aebe06682a2ab696a828eb99f88370dcd214a
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-05-16 18:41:18 +0200

    Update jj.js

--- jj.js
+++ jj.js
@@ -1,1 +1,14 @@
-console.log(_);
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "goit-example-message";
+
+form.elements.message.value = localStorage.getItem(localStorageKey) ?? "";
+
+form.addEventListener("input", (evt) => {
+  localStorage.setItem(localStorageKey, evt.target.value);
+});
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
